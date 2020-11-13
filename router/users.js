b7caad12eaef3aacea0dbc07cb0b9ba5b093e65e
Vincent Li <zil007@ucsd.edu>
2020-11-13 21:52:42 +0000

    implement email verification

--- router/users.js
+++ router/users.js
@@ -1,5 +1,8 @@
 const db = require("../utils/database");
+const em = require("../utils/email");
 const router = require("express").Router();
+const crypto = require("crypto");
+const { Server } = require("http");
 
 router.use(require("body-parser").json());
 
@@ -10,7 +13,9 @@ router.post("/email_register", async (req, res) => {
     data = {
         email: req.body.email,
         userName: req.body.userName,
-        password: req.body.password
+        password: req.body.password,
+        verify: false,
+        token: crypto.randomBytes(16).toString('hex')
     };
     
     console.log("passin signup data is: ", data);
@@ -20,6 +25,9 @@ router.post("/email_register", async (req, res) => {
 
     if (!user) {//user's email not signed up, now sign up with email+password
         db.insertOne("Users", data);
+        em.sendEmail(data.email, "EZCampus Account Verification",
+            'Hello,\n\n' + 'Please verify your EZCampus account by clicking the link: \nhttp:\/\/'
+            + 'server.metaraw.world:3000' + '\/users\/' + '\/confirmation\/' + data.token + '.\n');
         res.status(200).json({ message: "User Created", statusCode: 200 });
     } else {
         res.status(403).json({ message: "User existed", statusCode: 403 });
@@ -50,6 +58,28 @@ router.post("/email_login", async (req, res) => {
     }    
 });
 
+router.get("/confirmation/:token", async ({ params: { token } }, res) => {
+    let user = await db.findOne("Users", { token: token }, { projection: { "_id": 0 } });
+    if (!user) {
+        res.status(404).send("user not existed!");
+    } else {
+        await db.updateOne("Users", { token: token }, { $set: { verify: true } });
+        res.status(200).send("success");
+    }
+});
+
+router.get("/resend_verify", async ({ query: { email } }, res) => {
+    let user = await db.findOne("Users", { email: email }, { projection: { "_id": 0 } });
+    if (!user) {
+        res.status(404).json({ statusCode: 404, message: "user does not exist" });
+    } else {
+        em.sendEmail(email, "EZCampus Account Verification",
+            'Hello,\n\n' + 'Please verify your EZCampus account by clicking the link: \nhttp:\/\/'
+            + 'server.metaraw.world:3000' + '\/users\/' + '\/confirmation\/' + user.token + '.\n');
+        res.status(200).json({ statusCode: 200, message: "success" });
+    }
+    
+});
 
 
 module.exports = router;
