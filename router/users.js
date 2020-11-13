9ae3c9587c1dfba7d368104d5eb04ed1c1c3ed06
Yanling <yah104@ucsd.edu>
2020-11-13 10:21:17 -0800

    updated signup&login

--- router/users.js
+++ router/users.js
@@ -8,20 +8,21 @@ router.post("/email_register", async (req, res) => {
     
     let data;
     data = {
-        email: req.body.user.email,
-        password: req.body.user.password
+        email: req.body.email,
+        userName: req.body.userName,
+        password: req.body.password
     };
     
-    console.log(data);
+    console.log("passin signup data is: ", data);
 
     let user = await db.findOne("Users", { email: data.email }, { projection: { "_id": 0 } });
-    console.log(user);
+    console.log('find signup user is: ', user);
 
     if (!user) {//user's email not signed up, now sign up with email+password
         db.insertOne("Users", data);
-        res.status(200).json({ message: "User Created" });
+        res.status(200).json({ message: "User Created", statusCode: 200 });
     } else {
-        res.status(409).json({ message: "User existed" });
+        res.status(403).json({ message: "User existed", statusCode: 403 });
     }    
 });
 
@@ -29,21 +30,22 @@ router.post("/email_register", async (req, res) => {
 router.post("/email_login", async (req, res) => { 
     let data;
     data = {
-        email: req.body.user.email,
-        password: req.body.user.password
+        email: req.body.email,
+        password: req.body.password
     };
-    console.log("data is: ", data);
+    console.log("passin login data is: ", data);
     let user = await db.findOne("Users", { email: data.email }, { projection: { "_id": 0 } });
-    console.log('user is: ', user);
+    console.log('find login user is: ', user);
 
     
     if (!user) {//user email not found in db
-        res.status(404).json({ message: "User Not Found" });
+        res.status(404).json({ message: "User Does Not Exist", statusCode: 404 });
     } else {//user email found in db
-        if(req.body.user.password==user.password){//password match
-            res.status(200).json({ message: "Correct Password" });
+        if(req.body.password==user.password){//password match
+            res.send({userName: user.userName, email: user.email});
+            res.status(200);
         } else {//password NOT match
-            res.status(401).json({ message: "Incorrect Password" });
+            res.status(403).json({ message: "Wrong Password", statusCode: 403 });
         }
     }    
 });
