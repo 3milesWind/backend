1833e74a3a6723d51a29becbf06e32ae3bf6945f
Yanling <yah104@ucsd.edu>
2020-11-13 11:50:41 -0800

    added posts.js
    create_a_post

---
+++ router/posts.js
@@ -0,0 +1,29 @@
+const db = require("../utils/database");
+const router = require("express").Router();
+
+router.use(require("body-parser").json());
+
+//create a post
+router.post("/create_a_post", async (req, res) => { 
+    let data;
+    data = {
+        postId: req.body.postId,
+        title: req.body.title,
+        description: req.body.description,
+        date: req.body.date,
+        views: req.body.views,
+        likes: req.body.likes,
+        creatorName: req.body.creatorName,
+        creatorEmail: req.body.creatorEmail,
+        postType: req.body.postType
+    };
+    console.log("passin create post data is: ", data);
+
+    db.insertOne("Posts", data);
+    res.status(200).json({ successful: true, message: "Post Created", statusCode: 200 });
+});
+
+
+
+
+module.exports = router;
